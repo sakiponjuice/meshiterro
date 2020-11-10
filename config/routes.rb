9df6e5417cf7c48e11b121d45c47169102f7cd74
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-10 09:01:56 +0000

    [Start] 4-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'homes#top'
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
